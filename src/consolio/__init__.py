fd81a449d8b77e8f2d5b973475986b1d0ef3cdef
Io.D (devcoons) <i_-_-_s@outlook.com>
2024-10-25 21:49:06 +0200

    add spinners

--- src/consolio/__init__.py
+++ src/consolio/__init__.py
@@ -1,5 +1,5 @@
 from .consolio import Consolio
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __name__ = "consolio"
 __all__ = ['Consolio']
--- src/consolio/consolio.py
+++ src/consolio/consolio.py
@@ -1,5 +1,7 @@
 import threading
 import time
+import sys
+import os
 
 class Consolio:
     # Color definitions
@@ -18,10 +20,27 @@ class Consolio:
     PROG_ERR = FG_RD + '[x] ' + RESET  # Error
     PROG_CMP = FG_GR + '[v] ' + RESET  # Complete
 
-    def __init__(self):
+    # Spinner definitions
+    SPINNERS = {
+        'star':  ['✶', '✸', '✹', '✺', '✹', '✷'],
+        'moon':  ['🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘'],
+        'snake': ['⠋', '⠙', '⠚', '⠞', '⠖', '⠦', '⠴', '⠲', '⠳', '⠓'],
+        'default': ['|', '/', '-', '\\']
+    }
+
+    def __init__(self, spinner_type='default'):
         self._animating = False
         self._spinner_thread = None
         self._lock = threading.Lock()
+
+        # Store the selected spinner type
+        self.spinner_type = spinner_type.lower()
+        self.spinner_chars = self.SPINNERS.get(self.spinner_type, self.SPINNERS['default'])
+
+        # Check if the spinner is supported
+        if not self.is_spinner_supported(self.spinner_chars):
+            self.spinner_type = 'default'
+            self.spinner_chars = self.SPINNERS['default']
 
     def sprint(self, indent, status, text):
         self.stop_animate()
@@ -44,18 +63,18 @@ class Consolio:
         self._spinner_thread.start()
 
     def _animate(self, indent):
-        spinner_chars = '|/-\\'
         idx = 0
         indent_spaces = " " * (indent * 4)
         while self._animating:
-            spinner_char = spinner_chars[idx % len(spinner_chars)]
+            spinner_char = self.spinner_chars[idx % len(self.spinner_chars)]
             line = f"{indent_spaces} {self.FG_MG}{spinner_char}{self.RESET}"
             with self._lock:
                 print(line, end='\r', flush=True)
             time.sleep(0.1)
             idx += 1
         with self._lock:
-            print(' ' * len(line), end='\r', flush=True)
+            clear_line = ' ' * (len(line))
+            print(f"{clear_line}", end='\r', flush=True)
 
     def stop_animate(self):
         if self._animating:
@@ -63,4 +82,13 @@ class Consolio:
             self._spinner_thread.join()
             self._spinner_thread = None
 
-
+    def is_spinner_supported(self, spinner_chars):
+        encoding = sys.stdout.encoding or 'utf-8'
+        for char in spinner_chars:
+            try:
+                char.encode(encoding)
+            except UnicodeEncodeError:
+                return False
+            except Exception:
+                return False
+        return True
